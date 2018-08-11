b6e7162545faebf8909a182d2892df5b417bfe67
Nathaniel Stiers <nvstiers@gmail.com>
2018-08-11 05:19:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(line, name){
 }
 
 function nowServing (line){
-  while (line.length >= 0) {
+  while (line.length > -1) {
     return `Currently serving ${line[0]}.`
     line.shift();
   }

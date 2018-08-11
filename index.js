c831b349664abdf8716c8fd13b5501856a4a815d
Nathaniel Stiers <nvstiers@gmail.com>
2018-08-11 04:52:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(array, name){
   array.push(name)
 
-  return `Welcome, ${name}. You are number ${${array.length} + 1}.`
+  return `Welcome, ${name}. You are number ${array.length} + ${1}.`
 }

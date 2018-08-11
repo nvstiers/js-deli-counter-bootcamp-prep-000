708ff777eef8e575aaf53c6ed341e98a19637638
Nathaniel Stiers <nvstiers@gmail.com>
2018-08-11 05:30:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function nowServing (line){
   }
   return "There is nobody waiting to be served!"
 }
+
+function currentLine(line){
+  if (line.length == 0) {
+    return "The line is currently empty."
+  }
+}

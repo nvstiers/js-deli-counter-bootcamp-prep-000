26aaabd0773a45fd15f43123d90de654d1514736
Nathaniel Stiers <nvstiers@gmail.com>
2018-08-11 05:15:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function takeANumber(line, name){
 function nowServing (line){
   while (line.length > 0) {
     return `Currently serving ${line[0]}.`
-    line.shift();
+
   }
+  line.shift();
   return "There is nobody waiting to be served!"
 }
